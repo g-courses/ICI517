51ec2eedfa0971bf1d1e6d889cc2efd5fcc4f462
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2022-04-21 22:42:47 -0400

    Add files via upload

---
+++ apuntes/03-simd-examples/01-load-store/include/global.hh
@@ -0,0 +1,14 @@
+#ifndef __GLOBAL__
+#define __GLOBAL__
+
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+#include <RandomUnifStream.hpp>
+#include <Timer.hpp>
+
+
+#endif
---
+++ apuntes/03-simd-examples/01-load-store/main.cc
@@ -0,0 +1,55 @@
+#include <global.hh>
+
+// Intrinsics to use
+#include <emmintrin.h> 
+#include <immintrin.h> 
+#include <xmmintrin.h>
+
+
+int main(int argc, char** argv)
+{
+	__m128 dataRegister; //registro de float de 128 bits	
+	dataRegister = _mm_setzero_ps();
+
+	float* dataMemoryIn  = new float[4];
+	float* dataMemoryOut = new float[4];
+	
+	dataMemoryIn[0] = 0.5;
+	dataMemoryIn[1] = 1.5;
+	dataMemoryIn[2] = 2.5;
+	dataMemoryIn[3] = 3.5;
+	
+	// __m128 _mm_load_ps(float const* mem_addr)
+	// dst[127:0] := MEM[mem_addr+127:mem_addr]
+	dataRegister = _mm_load_ps(dataMemoryIn);
+
+	//__m128 _mm_mul_ps(__m128 a, __m128 b)
+	//	FOR j := 0 to 3
+	//		i := j*32
+	//		dst[i+31:i] := a[i+31:i] * b[i+31:i]
+	//	ENDFOR
+	dataRegister = _mm_mul_ps(dataRegister, dataRegister);
+
+	//__m128 _mm_rcp_ps (__m128 a)
+	//	FOR j := 0 to 3
+	//		i := j*32
+	//		dst[i+31:i] := SQRT(a[i+31:i])
+	//	ENDFOR		
+	dataRegister = _mm_rcp_ps(dataRegister);
+
+	// void _mm_store_ps (float* mem_addr, __m128 a)
+	// MEM[mem_addr+127:mem_addr] := a[127:0]
+	_mm_store_ps(dataMemoryOut, dataRegister);
+	
+	
+	//Visualizar el contenido del array de salida
+	for(size_t ii=0; ii < 4; ii++){
+		std::cout << "dataMemoryOut["<< ii <<"] = ";
+		std::cout << dataMemoryOut[ii];
+		std::cout << std::endl;
+
+	}
+	
+
+	return(EXIT_SUCCESS);
+}
