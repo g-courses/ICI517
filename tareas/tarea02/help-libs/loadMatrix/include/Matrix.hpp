e22bba12ea3db08f2e601586bcbe307da37ebdbd
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2022-04-25 23:56:50 -0400

    Add files via upload

---
+++ tareas/tarea02/help-libs/loadMatrix/include/Matrix.hpp
@@ -0,0 +1,167 @@
+#pragma once
+
+/////////////////////////////////////////////////////////////////////////////////
+//
+//   Uso:
+//   		Matrix varName(fileName);
+//
+//   Description:
+//                Carga los datos de una matriz 2d
+//                desde el archivo 'fileName'
+//                a memoria principal (variable 'varName')
+//
+//   Atributos públicos:
+//	             _nfil, _ncol   : filas, columnas de datos de la matriz
+//               _matrixInMemory: matriz en memoria.
+//               operador ()    : sobrecargado para acceder a los datos de la matriz (i,j)
+//               value(i,j)     : devuelve el elmente i,j
+//               value(i,j, val): asigna el valor val al elemento i,j
+//
+//  Última revisión: 2021-08-22
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <exception>
+
+struct matrixException : public std::exception {
+	std::string msg;
+	const char * what () const throw () {
+		return(msg.c_str()) ;
+	}
+};
+
+
+template <typename T>
+class Matrix{
+
+private:
+	std::ifstream _matrixFile;
+	
+	std::string _separador;
+	std::string _fileName;
+	
+	T *_matrixInMemory;
+	
+public:
+	uint32_t _nfil, _ncol;
+	
+	
+public:
+	Matrix(uint32_t nfil, uint32_t ncol);
+	Matrix(std::string fileName, std::string separador);
+	~Matrix();
+	
+	void load();
+	T operator()(uint32_t id_i, uint32_t id_j);
+	T value(uint32_t id_i, uint32_t id_j);
+	void value(uint32_t id_i, uint32_t id_j, T val);
+	
+	
+private:
+	void openFile();
+	void loadInMemory();
+
+};
+
+template <typename T>
+Matrix<T>::Matrix(uint32_t nfil, uint32_t ncol): _nfil(nfil), _ncol(ncol)
+{
+	_matrixInMemory = new T[ _nfil * _ncol ];
+}
+
+template <typename T>
+Matrix<T>::Matrix(std::string fileName, std::string separador): _fileName(fileName), _separador(separador)
+{
+
+}
+
+template <typename T>
+Matrix<T>::~Matrix()
+{
+	delete[] _matrixInMemory;
+}
+
+template <typename T>
+void Matrix<T>::load()
+{
+	this->openFile();
+	
+	this->loadInMemory();
+	
+}
+
+
+template <typename T>
+void Matrix<T>::openFile()
+{
+	_matrixFile.open(_fileName);
+	if(!_matrixFile.good()){
+		matrixException mException;
+		mException.msg= "Error en acceso a archivo: " + _fileName;
+		throw mException;
+	}
+}
+
+template <typename T>
+void Matrix<T>::loadInMemory()
+{
+	std::string line;
+	size_t index = 0;
+	
+	std::getline(_matrixFile, line);
+
+	index = line.find(_separador);
+	
+	_nfil    = std::stoi( line.substr(0, index) );
+	_ncol = std::stoi( line.substr(index + _separador.length(), line.length()) );
+	
+	_matrixInMemory = new T[ _nfil * _ncol ];
+	
+
+	for(size_t i = 0; i < _nfil; i++){
+		std::getline(_matrixFile, line);
+			
+		for(size_t j = 0; j < _ncol; j++){
+			index = line.find(_separador);
+			std::stringstream ssLine( line.substr(0, index)  );
+			T value;
+			ssLine >> value;
+
+			_matrixInMemory[i*_ncol + j] = value;
+			
+			line.erase(0, index + _separador.length());	
+		}
+	}
+	_matrixFile.close();
+}
+
+template <typename T>
+T Matrix<T>::operator()(uint32_t id_i, uint32_t id_j)
+{
+	return(_matrixInMemory[id_i*_ncol + id_j]);
+}
+
+template <typename T>
+T Matrix<T>::value(uint32_t id_i, uint32_t id_j)
+{
+	return(_matrixInMemory[id_i*_ncol + id_j]);
+}
+
+template <typename T>
+void Matrix<T>::value(uint32_t id_i, uint32_t id_j, T val)
+{
+	uint32_t idx = id_i*_ncol + id_j;
+	_matrixInMemory[idx] = val;
+}
+
+
+
+
+
+
+
+
+
---
+++ tareas/tarea02/help-libs/loadMatrix/main.cc
@@ -0,0 +1,62 @@
+#include <global.hh>
+
+#include <Timer.hpp>
+#include <Matrix.hpp>
+
+/////////////////////////////////////////////////////////////////////////////////
+//   Usage:
+//           ./matrix --A matrix_file  
+//
+//   Description:
+//           Programa driver que muestra el uso de la librería para cargar matrices (Matrix.hpp)
+//           y de medición de tiempo (Timer.hpp)
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+
+int main(int argc, char** argv)
+{
+
+	Timer<std::chrono::nanoseconds> timer1;
+	std::string fileMatrixA;
+	
+	///////////////////////////////////////
+	//  Read command-line parameters
+	if(argc != 3){
+		std::cerr << "Uso: " << argv[0] << " --A MATRIX_FILE" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	std::string mystr;
+	for (size_t i=0; i < argc; i++) {
+		mystr=argv[i];
+		if (mystr == "--A") {
+			fileMatrixA = argv[i+1];
+		}
+	}
+	
+	Matrix<double> m1(fileMatrixA, " ");
+	
+	timer1.start();
+	try{
+		m1.load();
+	} catch(std::exception& e){
+		std::cerr << e.what() <<std::endl;
+		exit(EXIT_FAILURE);
+	}
+	timer1.stop();
+	
+	std::cout << "Time to load matrix in memory: " << timer1.elapsed() << " ns\n";
+	
+	for(size_t i=0; i< m1._nfil; i++){
+		for(size_t j=0; j< m1._ncol; j++){
+			std::cout << m1(i,j) << "\t";
+		}
+		std::cout << std::endl;
+	}
+	
+	
+	
+	return(EXIT_SUCCESS);
+}
+
+
